d3c7a25d76a98a9cd3d5a812fd5b4710b3c9a3ef
Rets, Dmitriy <Dmitriy.Rets@nexign.com>
2021-10-14 11:47:15 +0300

    complete and debugged

--- main.go
+++ main.go
@@ -16,14 +16,14 @@ type Print struct {
 }
 
 var (
-	eps = 1e-6
+	eps = 1e-5
 )
 
 func fFunc(x, u, v float64) float64 {
 	return u - v + math.Exp(x)*(1+x*x)
 }
 func gFunc(x, u, v float64) float64 {
-	return u + v + x + math.Exp(x)
+	return u + v + x*math.Exp(x)
 }
 func uFunc(x float64) float64 {
 	return x * math.Exp(x)
@@ -46,7 +46,6 @@ func step(x, u, v, h float64) (float64, float64) {
 
 	phi3 := h * fFunc(x+h, u+phi2, v+psi2)
 	psi3 := h * gFunc(x+h, u+phi2, v+psi2)
-
 	u = u + (phi0+2*phi1+2*phi2+phi3)/6
 	v = v + (psi0+2*psi1+2*psi2+psi3)/6
 	return u, v
@@ -61,7 +60,9 @@ func jump(x, u, v, h0 float64) (float64, float64, float64) {
 	sU1, sV1 := step(x, u, v, h)
 	sU05, sV05 := step(x, u, v, h/2)
 	sU2, sV2 := step(x+h/2, sU05, sV05, h/2)
+	fmt.Println(h, math.Abs(sU1-sU2) > eps, math.Abs(sV1-sV2) > eps, math.Abs(sV1-sV2), sV1, sV2)
 	for math.Abs(sU1-sU2) > eps && math.Abs(sV1-sV2) > eps {
+		fmt.Println(h, math.Abs(sU1-sU2) > eps, math.Abs(sV1-sV2) > eps)
 		h = h / 2
 		sU1 = sU05
 		sV1 = sV05
